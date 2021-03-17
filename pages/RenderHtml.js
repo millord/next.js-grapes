ee07b33fa85c188bd6f591dc7f1530bc96b8d0bd
millord <juancarlosmillord@gmail.com>
2021-03-17 16:30:08 -0400

    fixed margin top bug in grapes editor

--- pages/RenderHtml.js
+++ pages/RenderHtml.js
@@ -35,11 +35,11 @@ import React, {useState} from 'react'
    </title>
    <meta charSet="UTF-8"/>
    <meta name="viewport" content="width=device-width, initial-scale=1"/>
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"/>
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato"/>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"/>
+   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato"/>
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
 
-<div className="" >
+  <div className="navSection"  >
   <div className="w3-bar w3-black w3-card">
     <a href="javascript:void(0)" onClick="myFunction()" title="Toggle Navigation Menu" className="w3-bar-item w3-button w3-padding-large w3-hide-medium w3-hide-large w3-right"><i className="fa fa-bars">
       </i></a>
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 // import '../styles/globals.css'
 
 import 'grapesjs/dist/css/grapes.min.css';
-
 import "./styles.css"
-
 import "./index.css"
+
+
+
+
 
 
 
